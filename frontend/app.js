db9fca3fe5d7bbff74c9142ddfb99c64624864ac
Flavio Grilli <flavio.grilli@ynov.com>
2026-04-28 12:28:31 +0200

    feat : frontend server routes

--- frontend/app.js
+++ frontend/app.js
@@ -6,6 +6,7 @@ const port = 8000
 
 app.use(cors({origin: '*'}))
 app.use(express.json())
+app.use(express.static(__dirname))
 
 app.use(router)
 
--- frontend/router/yshop.js
+++ frontend/router/yshop.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const path = require('path')
 
+// pathJoin permet de relier le chemin absolu (__dirname) et le chemin relatif (filePath) pour faciliter l'écriture des chemins pour les routes.
+function pathJoin(filePath) {
+    return path.join(__dirname, filePath)
+}
+
+router.get('/', (req, res) => {
+    res.sendFile(pathJoin('../index.html'))
+})
+
+// On utilise router.use(...) sans spécifier de chemin pour intercepter tous les chemins. On renvoie donc tous les chemins inconnus sur une erreur 404.
+router.use((req, res) => {
+    res.sendFile(pathJoin('../test.html'))
+})
 
 module.exports = router
